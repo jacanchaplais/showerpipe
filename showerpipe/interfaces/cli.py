32609bffb3ebea52abbb231ff0a8e4b0f5131a87
Jacan Chaplais <jlc1n20@soton.ac.uk>
2022-01-17 10:53:30 +0000

    added pythia subcommand for extensibility

--- showerpipe/interfaces/cli.py
+++ showerpipe/interfaces/cli.py
@@ -5,19 +5,40 @@ from showerpipe.interfaces._subject import ShowerPipeline
 from showerpipe.interfaces import observer
 
 
-@click.command()
+def run(pipeline, listeners):
+    """Attaches the observers to the event generator, and then runs it."""
+    for listen in listeners: pipeline.attach(listen)
+    with click.progressbar(pipeline) as events:
+        for event in events: event.notify()
+
+@click.group()
+@click.option(
+        '--hdf-path', type=click.Path(dir_okay=False, writable=True),
+        default='',
+        help='save as hdf at specified path (extension .h5 or .hdf5)'
+        )
+@click.pass_context
+def showergen(ctx, hdf_path):
+    """Generate Monte-Carlo hadronisation and particle shower data
+    in an extensible pipeline.
+    """
+    ctx.obj = {}
+    ctx.obj['listeners'] = [] # prevent ns collision with observer module
+    if hdf_path != '':
+        ctx.obj['listeners'].append(observer.HdfStorage(hdf_path))
+
+@showergen.command()
 @click.argument('settings-file', type=click.Path(exists=True))
-@click.option('--me-file', type=click.Path(exists=True))
-@click.option('--hdf/--no-hdf', default=False)
-def main(settings_file, me_file, hdf):
+@click.option(
+        '--me-file', type=click.Path(exists=True),
+        help='Les Houches file containing hard events.'
+        )
+@click.pass_context
+def pythia(ctx, settings_file, me_file):
+    """Runs Pythia using SETTINGS_FILE, typically with extension 'cmnd'."""
     data_generator = PythiaGenerator(
             config_file=settings_file,
             me_file=me_file,
             )
     pipeline = ShowerPipeline(data_generator)
-    if hdf:
-        hdf_observer = observer.HdfStorage()
-        pipeline.attach(hdf_observer)
-    with click.progressbar(pipeline) as events:
-        for event in events:
-            event.notify()
+    run(pipeline, ctx.obj['listeners'])
