9f276de22cc69dbb146c70c33a96fb7616d2e5e7
Jacan Chaplais <jlc1n20@soton.ac.uk>
2022-01-17 10:53:07 +0000

    added rest of data and removed silly defaults

--- showerpipe/interfaces/observer.py
+++ showerpipe/interfaces/observer.py
@@ -4,18 +4,20 @@ from showerpipe.interfaces._base import DataObserver
 
 
 class HdfStorage(DataObserver):
-    def __init__(self):
+    def __init__(self, path: str):
         from heparchy.write.hdf import HdfWriter
         self.__stack = ExitStack()
         self.__file_obj = self.__stack.enter_context(
-                HdfWriter(path='/home/jlc1n20/messy/silly.hdf5'))
+                HdfWriter(path=path))
         self.__process = self.__stack.enter_context(
-                self.__file_obj.new_process(name='top'))
+                self.__file_obj.new_process(name='default'))
 
     def update(self, data):
         with self.__process.new_event() as event:
             event.set_pmu(data.pmu)
+            event.set_color(data.color)
             event.set_edges(data.edges)
+            event.set_pdg(data.pdg)
 
     def close(self):
         self.__stack.close()
