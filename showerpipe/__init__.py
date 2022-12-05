dd3a469168fd67777499b82cae86fc3842a1785d
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-12-05 14:15:20 +0000

    patched missing exports for docs

--- showerpipe/__init__.py
+++ showerpipe/__init__.py
@@ -7,6 +7,8 @@ event generation programs for Pythonic usage, and exposing the data via
 NumPy arrays.
 """
 from ._version import __version__
+from . import generator
+from . import lhe
 
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "generator", "lhe"]
